330fe9168b6d59339aa83bec48b0e34a71240c96
OmerTheProgrammer <omer17508@gmail.com>
2025-12-14 22:48:44 +0200

    added notes

--- answer_bot.py
+++ answer_bot.py
@@ -1,7 +1,10 @@
 import os
 import google.generativeai as genai #עובד למרות השגיאה
 
+#https://aistudio.google.com/app/api-keys?hl=he&_gl=1*1wep0tg*_ga*NDUwMjIzMTIuMTcyNTgxMjI2NQ..*_ga_P1DBVKWT6V*czE3NjU3NDUyNjUkbzUkZzAkdDE3NjU3NDUyNjUkajYwJGwwJGgzNTA1MzI2Nw..
+#אתר ליצירת מפתחות
 genai.configure(api_key='AIzaSyAbOKpLLidrCeBUqMMVwhW7k7eFnr8H4nw')#ליצור מפתח כל שימוש
+# המפתח הזה נמחק כבר
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 request = input("what's the request: ")
