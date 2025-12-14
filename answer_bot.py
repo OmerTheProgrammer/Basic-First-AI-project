5f1bbfadcb9be20776e304afd0ad04bc9c6806ec
OmerTheProgrammer <omer17508@gmail.com>
2025-12-14 23:11:00 +0200

    fixed "data leak"

--- answer_bot.py
+++ answer_bot.py
@@ -3,7 +3,7 @@ import google.generativeai as genai #עובד למרות השגיאה
 
 #https://aistudio.google.com/app/api-keys?hl=he&_gl=1*1wep0tg*_ga*NDUwMjIzMTIuMTcyNTgxMjI2NQ..*_ga_P1DBVKWT6V*czE3NjU3NDUyNjUkbzUkZzAkdDE3NjU3NDUyNjUkajYwJGwwJGgzNTA1MzI2Nw..
 #אתר ליצירת מפתחות
-genai.configure(api_key='AIzaSyAbOKpLLidrCeBUqMMVwhW7k7eFnr8H4nw')#ליצור מפתח כל שימוש
+genai.configure(api_key='')#ליצור מפתח כל שימוש
 # המפתח הזה נמחק כבר
 
 model = genai.GenerativeModel("gemini-1.5-flash")
